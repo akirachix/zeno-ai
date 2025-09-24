05db96d82ee091252a653e18a594fbbba514306f
meronteklehaymanotgebru <meronteklehaymanotgebru@gmail.com>
2025-09-24 23:49:08 +0300

    created zeno agent

--- zeno_agent/main.py
+++ zeno_agent/main.py
@@ -1,10 +1,1 @@
-from fastapi import FastAPI
-from .web_api import app as custom_app
-
-def create_app() -> FastAPI:
-    app = FastAPI()
-    app.mount("/", custom_app)
-    print("✅ Mounted at root")
-    return app
-
-app = create_app()
+from .web_api import app
